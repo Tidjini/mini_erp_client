dd53a0780c44b38ca65c8b9c0018a8551e130fc5
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-12 12:13:31 +0100

    set signin with token

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -3,13 +3,23 @@ import axios from "axios";
 import FuseUtils from "@fuse/FuseUtils";
 
 class Service extends FuseUtils.EventEmitter {
-  constructor(props) {
-    this.API_URL = process.env.API_URL;
-  }
-
   createUser = (data) => {
     return new Promise((resolve, reject) => {
-      axios.post(`${this.API_URL}/utilisateurs/`, data).then((response) => {
+      axios
+        .post(`${process.env.API_URL}/utilisateurs/`, data)
+        .then((response) => {
+          if (response.data) {
+            this.setSession(response.data.token);
+            resolve(response.data);
+          } else {
+            reject(response.data.error);
+          }
+        });
+    });
+  };
+  signIn = (creadential) => {
+    return new Promise((resolve, reject) => {
+      axios.post(`${process.env.AUTH_USER_URL}/`, data).then((response) => {
         if (response.data) {
           this.setSession(response.data.token);
           resolve(response.data);
@@ -19,12 +29,16 @@ class Service extends FuseUtils.EventEmitter {
       });
     });
   };
-  signIn = (creadential) => {
+
+  signInToken = () => {
     return new Promise((resolve, reject) => {
-      axios.post(`${process.env.AUTH_USER_URL}/`, data).then((response) => {
-        if (response.data) {
+      axios.defaults.headers.common["Authorization"] =
+        "token " + this.getAccessToken();
+
+      axios.get(`${process.env.AUTH_TOKEN_URL}/`, {}).then((response) => {
+        if (response.data.utilisateur) {
           this.setSession(response.data.token);
-          resolve(response.data);
+          resolve(response.data.utilisateur);
         } else {
           reject(response.data.error);
         }
