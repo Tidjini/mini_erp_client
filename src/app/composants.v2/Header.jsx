ff42fa43b5e9040b8006a3d1cf288f85bfa524fd
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-19 11:59:40 +0100

    set header with change mode from xl to xs

--- src/app/composants.v2/Header.jsx
+++ src/app/composants.v2/Header.jsx
@@ -6,20 +6,46 @@ import useBreakPoints from "app/hooks/useBreakPoints";
 
 export default function Header(props) {
   const { icon, title } = props;
-
   const size = useBreakPoints();
+
+  const [iconSize, setIconSize] = React.useState({
+    width: 36,
+    height: 36,
+  });
+  const [titleSize, setTitleSize] = React.useState(16);
+
+  React.useEffect(() => {
+    function changeDisplaySize() {
+      switch (size) {
+      }
+      switch (size) {
+        case "xs":
+        case "sm":
+          setIconSize({ width: 36, height: 36 });
+          setTitleSize(16);
+          break;
+
+        default:
+          setIconSize({ width: 56, height: 56 });
+          setTitleSize(21);
+          break;
+      }
+    }
+    changeDisplaySize();
+  }, [size]);
 
   return (
     <div style={{ display: "flex", alignItems: "center", margin: 16 }}>
       <img
         src={icon || "assets/images/logos/logo.svg"}
-        style={{ height: 36, width: 36 }}
+        style={{ ...iconSize }}
       />
 
-      <Typography style={{ fontSize: 16, marginLeft: 14, fontWeight: "600" }}>
+      <Typography
+        style={{ fontSize: titleSize, marginLeft: 14, fontWeight: "600" }}
+      >
         {title}
       </Typography>
-      <Typography>{"w:" + size + ", h:" + size}</Typography>
     </div>
   );
 }
