145312ff805e3603952b332e2a86fb2340830a77
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-20 13:37:19 +0100

    clean Collection Body

--- src/app/tasks/TaskRow.jsx
+++ src/app/tasks/TaskRow.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 //thirds
-import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import { withStyles } from "@material-ui/core/styles";
-//application
-import { forecolors } from "../constants";
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
@@ -14,24 +11,15 @@ const StyledTableRow = withStyles((theme) => ({
     },
   },
 }))(TableRow);
-
 const StyledTableCell = withStyles((theme) => ({
   body: {
     fontSize: 14,
   },
 }))(TableCell);
 
-export default function CollectionBody(props) {
-  const { data, cells, eventHandlers, children } = props;
-  return (
-    <TableBody>
-      {data && data.map((row) => setRow(children, row, cells, eventHandlers))}
-    </TableBody>
-  );
-}
+export default function TaskRow(props) {
+  const { data } = props;
 
-function setRow(CustomRow, item, cells, eventHandlers, pk = "id") {
-  if (Boolean(CustomRow)) return <CustomRow data={item} />;
   return (
     <StyledTableRow key={item[pk]} {...eventHandlers}>
       {cells &&
