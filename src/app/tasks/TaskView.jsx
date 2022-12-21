2b336b440e9ed2c2ff29de785db614e9fd37cf22
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-21 10:51:20 +0100

    set Title View Dynamic

--- src/app/tasks/TaskView.jsx
+++ src/app/tasks/TaskView.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import Header from "app/composants.v2/Header";
+import CollectionActions from "app/composants.v2/collection/CollectionActions";
+import { margins } from "app/composants.v2/constants";
 
-export default function TaskView() {
-  return <div>TaskView</div>;
+export default function TaskView(props) {
+  const { params } = props.match;
+
+  const [title, setTitle] = React.useState("Nouvelle Tâche");
+
+  React.useEffect(() => {
+    function handleTitle() {
+      if (!Boolean(params)) {
+        setTitle("Nouvelle Tâche");
+        return;
+      }
+      const { id } = params;
+      if (id === "nouveau") {
+        setTitle("Nouvelle Tâche");
+      } else {
+        setTitle(`Edition Tâche (${id})`);
+      }
+    }
+
+    handleTitle();
+  }, [params]);
+
+  return (
+    <div style={{ margin: margins.default }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Header title={title} />
+        <CollectionActions actions={[]} />
+      </div>
+    </div>
+  );
 }
