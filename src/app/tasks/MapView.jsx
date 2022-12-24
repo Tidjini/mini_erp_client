190463f227b1afa3a661285e57616c22c4298eb6
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-24 11:19:56 +0100

    drawing directions with success

--- src/app/tasks/MapView.jsx
+++ src/app/tasks/MapView.jsx
@@ -6,6 +6,7 @@ import {
   Marker,
   DirectionsService,
   DirectionsRenderer,
+  Polyline,
 } from "@react-google-maps/api";
 import { Typography } from "@material-ui/core";
 
@@ -27,14 +28,68 @@ const options = (google) => {
   };
 };
 export default function MapView() {
-  const [res, setRes] = React.useState();
+  const [directions, setDirections] = React.useState();
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY, // ,
     // ...otherOptions
   });
 
+  let directionsService;
+
+  function changeDirections(maps, depart, arrive) {
+    if (depart === null) return;
+    if (arrive === null) return;
+
+    directionsService = new maps.DirectionsService();
+    const origin = {
+      lat: depart.lat,
+      lng: depart.lng,
+    };
+    const destination = {
+      lat: arrive.lat,
+      lng: arrive.lng,
+    };
+    directionsService.route(
+      {
+        origin: origin,
+        destination: destination,
+        travelMode: window.google.maps.TravelMode.DRIVING,
+        provideRouteAlternatives: true,
+        optimizeWaypoints: true,
+      },
+      (result, status) => {
+        if (status === window.google.maps.DirectionsStatus.OK) {
+          // setDistance(result.routes[0].legs[0].distance.text);
+          // setDuration(result.routes[0].legs[0].duration.text);
+          // setDistanceM(result.routes[0].legs[0].distance.value);
+          // setDurationS(result.routes[0].legs[0].duration.value);
+          // hideElements(form.max_poid, result.routes[0].legs[0].distance.value);
+
+          setDirections(result);
+        } else {
+          console.error(`error fetching directions ${result}`);
+          console.log("SD, service", result);
+        }
+      }
+    );
+  }
+
   const renderMap = () => {
-    function onLoad(mapInstance) {}
+    function onLoad(mapInstance) {
+      // console.log(mapInstance);
+      // console.log(window);
+      changeDirections(
+        window.google.maps,
+        {
+          lat: 35.7279158,
+          lng: -0.5875089,
+        },
+        {
+          lat: 35.69856,
+          lng: -0.618288,
+        }
+      );
+    }
 
     return (
       <GoogleMap
@@ -97,7 +152,7 @@ export default function MapView() {
           }}
         />
 
-        <DirectionsService
+        {/* <DirectionsService
           // required
           options={{
             // eslint-disable-line react-perf/jsx-no-new-object-as-prop
@@ -137,31 +192,44 @@ export default function MapView() {
               directionsService
             );
           }}
-        >
-          {res !== null && (
-            <DirectionsRenderer
-              // required
-              options={{
-                // eslint-disable-line react-perf/jsx-no-new-object-as-prop
-                directions: res,
-              }}
-              // optional
-              onLoad={(directionsRenderer) => {
-                console.log(
-                  "DirectionsRenderer onLoad directionsRenderer: ",
-                  directionsRenderer
-                );
-              }}
-              // optional
-              onUnmount={(directionsRenderer) => {
-                console.log(
-                  "DirectionsRenderer onUnmount directionsRenderer: ",
-                  directionsRenderer
-                );
-              }}
-            />
-          )}
-        </DirectionsService>
+        > */}
+        {directions !== null && (
+          <DirectionsRenderer
+            directions={directions}
+            // required
+            // options={{
+            //   // eslint-disable-line react-perf/jsx-no-new-object-as-prop
+            //   directions: res,
+            // }}
+            // optional
+            onLoad={(directionsRenderer) => {
+              console.log(
+                "DirectionsRenderer onLoad directionsRenderer: ",
+                directionsRenderer
+              );
+            }}
+            // optional
+            onUnmount={(directionsRenderer) => {
+              console.log(
+                "DirectionsRenderer onUnmount directionsRenderer: ",
+                directionsRenderer
+              );
+            }}
+          />
+        )}
+        {directions && (
+          <Polyline
+            path={directions.routes[0].overview_path}
+            geodesic={true}
+            options={{
+              strokeColor: "#20BF55",
+              strokeOpacity: 0.8,
+              strokeWeight: 6,
+              clickable: true,
+            }}
+          />
+        )}
+        {/* </DirectionsService> */}
       </GoogleMap>
     );
   };
