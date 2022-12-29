f467905c1b1c9834c7d023f5ddd0955eb9a78893
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-29 12:22:18 +0100

    changes

--- src/@fuse/components/FuseLayout/FuseLayout.js
+++ src/@fuse/components/FuseLayout/FuseLayout.js
@@ -6,12 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import * as Actions from "app/store/actions";
 import { FuseLayouts } from "@fuse";
 import _ from "@lodash";
-import history from "@history";
 import AppContext from "app/AppContext";
-import { default as Notification } from "app/composants.v2/notification/Generic";
-import usePieSocket from "../../../app/hooks/services/usePieSocket";
-import ReactHowler from "react-howler";
-import { Button, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -81,116 +76,8 @@ function FuseLayout(props) {
   //
 
   const Layout = FuseLayouts[settings.layout.style];
-  const [open, setOpen] = React.useState(false);
-  const { task } = usePieSocket();
 
-  React.useEffect(() => {
-    if (task) {
-      setOpen(true);
-      if (howlerRef.current) {
-        howlerRef.current.seek(0);
-        howlerRef.current.play();
-      }
-    }
-  }, [task]);
-  const howlerRef = React.useRef(null);
-  const actions = (state) => {
-    const { statue, statue_label } = state;
-    const colors = {
-      fore: "#414962",
-      back: "#41496220",
-    };
-    switch (statue) {
-      case "a":
-        colors.fore = "#FFB703";
-        colors.back = "#FFB70320";
-
-        break;
-      case "p":
-        colors.fore = "#E76F51";
-        colors.back = "#E76F5120";
-        break;
-      case "t":
-        colors.fore = "#2A9D8F";
-        colors.back = "#2A9D8F20";
-        break;
-      case "c":
-        colors.fore = "#E63946";
-        colors.back = "#E6394620";
-        break;
-
-      default:
-        colors.fore = "#414962";
-        colors.back = "#41496220";
-        break;
-    }
-    return (
-      <div
-        style={{
-          paddingTop: 20,
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: 8,
-            backgroundColor: colors.back,
-            padding: "7px 20px",
-          }}
-        >
-          <Typography
-            style={{
-              fontSize: 14,
-              fontWeight: "600",
-              color: colors.fore,
-            }}
-          >
-            {statue_label}
-          </Typography>
-        </div>
-        <Button
-          style={{
-            backgroundColor: "#0a9396",
-            color: "white",
-            fontSize: 11,
-            textTransform: "none",
-          }}
-        >
-          Confirmation
-        </Button>
-      </div>
-    );
-  };
-  return (
-    <Layout classes={{ root: classes.root }} {...props}>
-      {task && (
-        <Notification
-          data={task}
-          open={open}
-          setOpen={setOpen}
-          onClick={(event) => {
-            if (task.id) {
-              history.push(`/task/${task.id}`);
-            }
-            setOpen(false);
-          }}
-        >
-          {actions(task.statue)}
-        </Notification>
-      )}
-      <ReactHowler
-        src="assets/sounds/notification-03.mp3"
-        ref={howlerRef}
-        playing={open}
-      />
-    </Layout>
-  );
+  return <Layout classes={{ root: classes.root }} {...props}></Layout>;
 }
 
 export default withRouter(React.memo(FuseLayout));
