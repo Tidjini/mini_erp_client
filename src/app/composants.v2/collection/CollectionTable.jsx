cb8faaa608848543a581975eda3efc2f119e3881
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-02 10:50:31 +0100

    set loading in table (test)

--- src/app/composants.v2/collection/CollectionTable.jsx
+++ src/app/composants.v2/collection/CollectionTable.jsx
@@ -6,6 +6,7 @@ import Paper from "@material-ui/core/Paper";
 import CollectionHeader from "app/composants.v2/collection/CollectionHeader";
 import CollectionBody from "./CollectionBody";
 import CollectionPagination from "./CollectionPagination";
+import { CircularProgress, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
   table: {
@@ -30,6 +31,16 @@ export default function CollectionTable(props) {
 
   return (
     <Paper style={{ ...style }}>
+      <div
+        className="flex flex-1 flex-col items-center justify-center  p-10"
+        style={{ backgroundColor: "#94d2bd20" }}
+      >
+        <Typography className="text-18 mb-16" color="textSecondary">
+          Loading...
+        </Typography>
+        {/* <LinearProgress className="w-xs" color="secondary" /> */}
+        <CircularProgress color="inherit" size={36} />
+      </div>
       <TableContainer style={{ maxHeight: 900 }}>
         <Table
           className={classes.table}
@@ -42,6 +53,7 @@ export default function CollectionTable(props) {
             order={order}
             onSort={onSort}
           />
+
           <CollectionBody
             cells={cells}
             data={data}
