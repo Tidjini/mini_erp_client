daf471371868b3a74a21cd88a4288cf83e0f6600
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-02 13:11:07 +0100

    map view clean and more style

--- src/app/tasks/TaskCollectionMapView.jsx
+++ src/app/tasks/TaskCollectionMapView.jsx
@@ -10,12 +10,10 @@ import TypedMarker from "app/composants.v2/map/Marker";
 import useDirections from "app/hooks/useDirections";
 
 import TaskMapInputs from "./TaskMapInputs";
-import CollectionActions from "app/composants.v2/collection/CollectionActions";
-import Action from "app/hooks/Action";
-import { backcolors } from "app/composants.v2/constants";
 import { useSelector } from "react-redux";
 import { useGetCollection } from "app/hooks/useRequest";
 import UtilisateurInfoWindow from "./UtilisateurInfoWindow";
+import useWindowSize from "app/hooks/useWindowSize";
 //center for oran
 const defaultCenter = {
   lat: 35.6976541,
@@ -25,6 +23,7 @@ const libs = ["places"];
 
 export default function TaskCollectionMapView({ onSave, path }) {
   const [maps, setMaps] = React.useState();
+  const { width, height } = useWindowSize();
   const [displayInfo, setDisplayInfo] = React.useState({
     display: false,
     position: {
@@ -55,30 +54,6 @@ export default function TaskCollectionMapView({ onSave, path }) {
     getUserCollection();
   }, [origin, destination]);
 
-  const saveAction = new Action(
-    "Sauvgarder",
-    () => {
-      if (origin && destination) {
-        let distance = 0;
-        let duration = 0;
-        if (directions) {
-          distance = directions.routes[0].legs[0].distance.value;
-          duration = directions.routes[0].legs[0].duration.value;
-        }
-
-        onSave && onSave(origin, destination, distance, duration);
-      }
-
-      setCenter(defaultCenter);
-      setOrigin(undefined);
-      setDestination(undefined);
-    },
-    "cancel",
-    {
-      backgroundColor: backcolors.add,
-    }
-  );
-
   React.useEffect(() => {
     if (path) {
       setOrigin({
@@ -95,32 +70,28 @@ export default function TaskCollectionMapView({ onSave, path }) {
   }, [path]);
 
   return (
-    <Grid
-      item
-      xl={12}
-      lg={12}
-      md={12}
-      sm={12}
-      xs={12}
-      style={{ paddingTop: 20 }}
-    >
-      <Paper style={{ width: "100%", borderRadius: 15 }}>
+    <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+      <Paper
+        style={{
+          width: "100%",
+          backgroundColor: "#edede950",
+        }}
+      >
         <Typography
           style={{
-            padding: 20,
-            fontSize: 18,
-            fontWeight: "700",
+            padding: "10px 20px",
+            fontSize: 14,
+            fontWeight: "600",
             width: "100%",
-            backgroundColor: "#26465350",
-            borderRadius: "15px 15px 0 0",
+            backgroundColor: "#edede9",
           }}
         >
-          Directions
+          Tâches Directions / Localisation
         </Typography>
         <div
           style={{
-            backgroundColor: "white",
             padding: "10px 20px 20px 15px",
+            width: "50%",
           }}
         >
           <TaskMapInputs
@@ -143,7 +114,7 @@ export default function TaskCollectionMapView({ onSave, path }) {
           loadError={loadError}
           style={{
             borderRadius: "0px 0px 15px 15px",
-            innerHeight: "100%",
+            height: height - 200,
           }}
           onLoad={(instance) => {
             setMaps(window.google.maps);
@@ -191,19 +162,6 @@ export default function TaskCollectionMapView({ onSave, path }) {
             onClick={(event) => {}}
             directions={directions}
           />
-          <div
-            style={{
-              marginTop: 20,
-              width: "100%",
-              borderRadius: 10,
-              display: "flex",
-              flexDirection: "row-reverse",
-            }}
-          >
-            {(user.is_admin || user.is_staff) && (
-              <CollectionActions actions={[saveAction]} />
-            )}
-          </div>
 
           <UtilisateurInfoWindow
             information={displayInfo}
