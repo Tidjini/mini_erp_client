a0a62e8b3e372f27622011a9cc6549e2cb44ec7b
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-08 11:46:49 +0100

    integrate Messages Panel in Right Side Layout 1

--- src/app/fuse-layouts/layout1/components/RightSideLayout1.js
+++ src/app/fuse-layouts/layout1/components/RightSideLayout1.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import QuickPanel from 'app/fuse-layouts/shared-components/quickPanel/QuickPanel';
+import React from "react";
+import QuickPanel from "app/fuse-layouts/shared-components/quickPanel/QuickPanel";
 
-function RightSideLayout1(props)
-{
-    return (
-        <React.Fragment>
-            <QuickPanel/>
-        </React.Fragment>
-    );
+import MessagesPanel from "app/messagrie/MessagesPanel";
+
+function RightSideLayout1(props) {
+  return (
+    <React.Fragment>
+      <QuickPanel />
+      <MessagesPanel />
+    </React.Fragment>
+  );
 }
 
 export default RightSideLayout1;
