0a8368bc8e06392f666be6035aa1dbba61d76193
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-08 12:58:32 +0100

    set tooltip for users (name indecator)

--- src/app/messagrie/ProfileCollection.jsx
+++ src/app/messagrie/ProfileCollection.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { useCollectionData } from "app/hooks/common/useCollectionData";
 import UserAvatar from "app/fuse-layouts/shared-components/UserAvatar";
-import { Badge } from "@material-ui/core";
+import { Badge, Tooltip } from "@material-ui/core";
 import { useUserStateColor } from "app/hooks/useUserStateInfo";
 
-function ProfileAvatar({ item }) {
-  const { stateInfo } = useUserStateColor(item.statue);
+function ProfileAvatar({ profile }) {
+  const { stateInfo } = useUserStateColor(profile.statue);
 
   return (
     <div style={{ width: 56, height: 56, padding: 6 }}>
-      <div style={{ position: "relative" }}>
-        <UserAvatar name={item.name} />
-        <Badge
-          style={{
-            position: "absolute",
-            bottom: "2px",
-            right: "5px",
-            backgroundColor: "#e76f51",
-            width: 11,
-            height: 11,
-            border: "2px solid #FFF",
-            borderRadius: 8,
-            ...stateInfo,
-          }}
-        />
-      </div>
+      <Tooltip title={profile.name}>
+        <div style={{ position: "relative" }}>
+          <UserAvatar name={profile.name} />
+          <Badge
+            style={{
+              position: "absolute",
+              bottom: "2px",
+              right: "5px",
+              backgroundColor: "#e76f51",
+              width: 11,
+              height: 11,
+              border: "2px solid #FFF",
+              borderRadius: 8,
+              ...stateInfo,
+            }}
+          />
+        </div>
+      </Tooltip>
     </div>
   );
 }
@@ -39,7 +41,7 @@ export default function ProfileCollection() {
   return (
     <div>
       {profiles.map((item, index) => (
-        <ProfileAvatar key={index} item={item} />
+        <ProfileAvatar key={index} profile={item} />
       ))}
     </div>
   );
