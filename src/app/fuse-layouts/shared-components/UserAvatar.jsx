9d1fb2ca7cbbdf20d893603517cb9ef1ba96d3af
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-08 12:36:49 +0100

    set onClick event in User Avatar

--- src/app/fuse-layouts/shared-components/UserAvatar.jsx
+++ src/app/fuse-layouts/shared-components/UserAvatar.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 
 export default function UserAvatar(props) {
-  const { picture, name } = props;
+  const { picture, name, onClick } = props;
 
   if (picture)
-    return <Avatar className="" alt={name ? name : "user"} src={picture} />;
-  return <Avatar className="">{name[0].toUpperCase()}</Avatar>;
+    return (
+      <Avatar
+        className=""
+        alt={name ? name : "user"}
+        src={picture}
+        onClick={onClick}
+      />
+    );
+  return (
+    <Avatar className="" onClick={onClick}>
+      {name[0].toUpperCase()}
+    </Avatar>
+  );
 }
