503b7f7b04af85d846310d33d3766ebb2fc68dbb
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-08 13:03:54 +0100

    separate panel actions

--- src/app/messagrie/store/actions/index.js
+++ src/app/messagrie/store/actions/index.js
@@ -1,7 +1,1 @@
-export const TOGGLE_MESSAGES_PANEL = "[MESSAGES PANEL] TOGGLE MESSAGES PANEL";
-
-export function toggleMessagesPanel() {
-  return {
-    type: TOGGLE_MESSAGES_PANEL,
-  };
-}
+export * from "./panel";
---
+++ src/app/messagrie/store/actions/panel.js
@@ -0,0 +1,7 @@
+export const TOGGLE_MESSAGES_PANEL = "[MESSAGES PANEL] TOGGLE MESSAGES PANEL";
+
+export function toggleMessagesPanel() {
+  return {
+    type: TOGGLE_MESSAGES_PANEL,
+  };
+}
