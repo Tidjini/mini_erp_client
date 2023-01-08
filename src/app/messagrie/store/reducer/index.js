6a7d214f7932e670c9daab30088ef0ed1d8d0a0d
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-08 11:36:36 +0100

    set reducer for open (close) state of message panel

---
+++ src/app/messagrie/store/reducer/index.js
@@ -0,0 +1,22 @@
+import * as Actions from "../actions";
+
+const initialState = {
+  open: false,
+};
+
+const messagePanel = function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case Actions.TOGGLE_MESSAGES_PANEL: {
+      const open = !state.open;
+      return {
+        open: open,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default messagePanel;
